9530ccfcc403b0ea9d9ed05f5512d3cd0d3450cb
jillsaw <uiharu4510471@gmail.com>
2024-10-16 22:14:51 +0900

    課題提出

--- laravel-basic/app/Http/Controllers/PostController.php
+++ laravel-basic/app/Http/Controllers/PostController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller{
     public function index() {
-
-    return view ('posts.index');    
+    $posts=DB::table('posts')->get();
+    return view ('posts.index',compact('posts'));    
     }
 }
